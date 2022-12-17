bdd3fe282694ff2b5c3f56b649963fa5dd315e1d
fridosamt <119745974+fridosamt@users.noreply.github.com>
2022-12-17 14:06:10 +0900

    contactform update
    contactform update, php test

--- contactform.php
+++ contactform.php
@@ -1,17 +1,51 @@
 <?php
 
-if (isset($_POST["submit"])) {
-  $fname = $_POST["name"];
+  $name = $_POST["name"];
   $email = $_POST["email"];
   $subject = $_POST["subject"];
 
- $mailTo = "fridolin.samtleben@posteo.de";
- $headers = "From: ".$email;
- $txt = "You have received an email from ".$fname.".\n\n".$subject;
+  function filter_email_header($form_field) {
+  return preg_replace('/[nr|!/<>^$%*&]+/','',$form_field);
+  }
 
-  mail($mailTo, $txt, $headers);
-  header("Location: index.php?mailsend");
+  $email = filter_email_header($email);
+
+ $mailTo = "fridolin.samtleben@posteo.de";
+ $headers = "From: $email";
+ $txt = "You have received an email from ".$name.".\n\n".$subject;
+
+
+  $sent = mail($mailTo, $txt, $headers);
+
+
+  if ($sent) {
+
+?><html>
+<head>
+<title>thank you</title>
+</head>
+<body>
+<h1>thank you</h1>
+<p>thank you for your feedback!</p>
+</body>
+</html>
+
+<?php
+
+} else {
+?><html>
+<head>
+<title>something went wrong</title>
+</head>
+<body>
+<h1>something went wrong</h1>
+<p>could not send your feedback. please try again.</p>
+</body>
+</html>
+<?php
 }
+?>
+
 
 
  ?>
